4f930612c8cbe2b0d207cf85b13f47d4994cf949
TakeItEasy <semen.herasymiuk@gmail.com>
2024-10-07 01:12:37 +0600

    making adaptive

--- js/modal.js
+++ js/modal.js
@@ -16,3 +16,22 @@
     refs.modal.classList.toggle("is-open");
   }
 })();
+
+(() => {
+  const menu = {
+    // Додати атрибут data-modal-open на кнопку відкриття
+    openMenuBtn: document.querySelector("[menu-modal-open]"),
+    // Додати атрибут data-modal-close на кнопку закриття
+    closeMenuBtn: document.querySelector("[menu-modal-close]"),
+    // Додати атрибут data-modal на бекдроп модалки
+    modal: document.querySelector("[menu-modal]"),
+  };
+
+  menu.openMenuBtn.addEventListener("click", toggleModal);
+  menu.closeMenuBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
+    menu.modal.classList.toggle("is-open");
+  }
+})();
